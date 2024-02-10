0c9555feea3853599f82b85f0729898231641fba
laurenhg <laurenhg@gmail.com>
2024-02-10 20:12:24 +0100

    react handbags

---
+++ src/components/Tile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Tile({image, title, paragraph}) {
+    return (
+        <section>
+            {image ? (
+                <img src={image} alt={title} />
+            ):(
+                <>
+                    <h2>{title}</h2>
+                    {paragraph && <p> {paragraph}</p>}
+                    </>
+            )}
+        </section>
+    );
+            }
+export default Tile;
